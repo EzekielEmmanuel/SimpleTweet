c8697332fcc49cc291d9ec39b1755930442861f8
EzekielEmmanuel <ezekielemmanuel2000@gmail.com>
2020-10-18 01:06:40 -0500

    Simple Twitter Client

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcel;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -26,6 +29,7 @@ public class ComposeActivity extends AppCompatActivity {
 
     EditText etCompose;
     Button btnTweet;
+    TextView textView;
 
     TwitterClient client;
     @Override
@@ -37,6 +41,7 @@ public class ComposeActivity extends AppCompatActivity {
 
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
+        textView = findViewById(R.id.textView);
 
         //Set click listener on button
         btnTweet.setOnClickListener(new View.OnClickListener() {
@@ -77,6 +82,22 @@ public class ComposeActivity extends AppCompatActivity {
                 });
             }
         });
+        etCompose.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                int length = etCompose.length() + 1;
+                String convert = String.valueOf(length);
+                textView.setText(convert);
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
 
     }
-}
+
+    }
+
+
